0b486b490e6fc7d98407deecc8229cd1ace5de4f
juancarlos <jccm697@gmail.com>
2021-07-05 22:00:57 +0200

    segundo ejerccios fragment

--- app/src/main/java/com/cursokotlin/fragmentexample/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/fragmentexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cursokotlin.fragmentexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.cursokotlin.fragmentexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() ,OnFragmentActionsListener {
@@ -12,9 +13,26 @@ class MainActivity : AppCompatActivity() ,OnFragmentActionsListener {
         super.onCreate(savedInstanceState)
        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+       binding.btnRed.setOnClickListener { loadFragment(RedFragment()) }
+        binding.btnBlue.setOnClickListener { replaceFragment(BlueFragment()) }
+
     }
 
     override fun onClickFragmentButton() {
         Toast.makeText(this, "El botón ha sido pulsado", Toast.LENGTH_LONG).show()
+
     }
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
 }
